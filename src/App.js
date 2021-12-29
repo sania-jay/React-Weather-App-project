9fd1fb3020e77a53bc76d6c6f5f376874b50ca6f
sania-jay <92186934+sania-jay@users.noreply.github.com>
2021-12-29 12:22:03 +0000

    added footer with Github link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+     <h1>Weather App</h1> 
+
+<p>built by Sania Javed and <a href="https://github.com/sania-jay/React-Weather-App-project">open-sourced on Github</a>  </p>
     </div>
   );
 }
