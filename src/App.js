9d1a9b1a55398092308ee31d9c48f69bd7920ce9
sania-jay <92186934+sania-jay@users.noreply.github.com>
2022-01-05 13:16:02 +0000

    added rel="noreferrer" with target_blank

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-    
-    <Weather defaultCity="Tokyo" />
-     
-<footer>
-  Coded by Sania Jay and <a href="https://github.com/sania-jay/React-Weather-App-project" target="_blank">open-sourced on Github </a>
-  </footer>  
+      <Weather defaultCity="Tokyo" />
+
+      <footer>
+        Coded by Sania Jay and{" "}
+        <a
+          href="https://github.com/sania-jay/React-Weather-App-project"
+          target="_blank"
+          rel="noreferrer">
+          open-sourced on Github{" "}
+        </a>
+      </footer>
     </div>
   );
 }
